0f03576bfed9304ad0b1348550c5798f8b3e490a
yusheng116 <a0977013382@gmail.com>
2018-12-17 13:57:02 +0800

    Chapter 6 finished

---
+++ blog/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def main(request):
+    '''
+    Render the main page
+    '''
+    context = {'like':'Django 很棒'}
+    return render(request, 'main/main.html', context)
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
